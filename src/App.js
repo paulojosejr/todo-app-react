1b29f23bd65155a3f13c533a3af2b4fff44298b8
Paulo Araújo <paulojosejr95@gmail.com>
2020-11-25 11:19:38 +0000

    components form and todolist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+//Importing Components
+import Form from './components/Form'
+import TodoList from './components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState("")
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Paulo's Todo List</h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
