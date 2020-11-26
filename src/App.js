804675df08c2157740be9a4c7cb14c912603afad
Paulo Araújo <paulojosejr95@gmail.com>
2020-11-26 11:26:26 +0000

    adding components, events and items

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import TodoList from './components/TodoList'
 
 function App() {
   const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <header>
         <h1>Paulo's Todo List</h1>
       </header>
-      <Form setInputText={setInputText} />
-      <TodoList />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList todos={todos}/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,23 @@
 import React from 'react'
 
 
-const Form = ({ setInputText }) => {
+const Form = ({ setInputText,todos,setTodos,inputText }) => {
     //Here I can write javascript code and function
     const inputTextHandler = (e) => {
         console.log(e.target.value)
         setInputText(e.target.value)
     }
+    const submitTodoHandler = (e) => {
+        e.preventDefault()
+        setTodos([
+            ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
+        ])
+        setInputText("")
+    }
     return (
         <form>
-            <input onChange={inputTextHandler} type="text" className="todo-input" />
-            <button className="todo-button" type="submit">
+            <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
